0a1bbb8ec59e498eeb6e899e43580ff82355c5b7
mechawomen <45677066+mechawomen@users.noreply.github.com>
2018-12-06 22:43:26 -0800

    Update PHOTORESISTORS.ino

--- PHOTORESISTORS.ino
+++ PHOTORESISTORS.ino
@@ -1,102 +1,83 @@
-//PHOTORESISTORS
-  const int ldrpin = A0;
- 
+const int ldrpin = A0;
+const int ldrpin1 = A1;
+const int LED = 13;
 
-//LED
-  const int LED = 13;
-  
 //MOTOR
-  const int ENA = 7;
-  const int In1 = 8;
-  const int In2 = 9;
-  int SPEED = 100; // scale is 0 to 255
+const int ENA = 6   ;
+const int In1 = 8;
+const int In2 = 9;
+int SPEED = 100; // scale is 0 to 255
   
 void setup() {
-  Serial.begin(9600);  //Begin serial communcation
-  //initialize LDRs and LED
-  pinMode(ldrpin, INPUT);
-  pinMode(LED,OUTPUT);
+  //LDRS AND LED
+  Serial.begin(9600);
+   pinMode(ldrpin, INPUT);
+   pinMode(ldrpin1, INPUT);
+   pinMode(LED, OUTPUT);
 
-  //initialize MOTOR
-  pinMode(In1, OUTPUT);
-  pinMode(In2, OUTPUT);
-  pinMode(ENA, OUTPUT);
+   //MOTOR
+   pinMode(In1, OUTPUT);
+   pinMode(In2, OUTPUT);
+   pinMode(ENA, OUTPUT);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
     Serial.print("HELLO");
     Serial.print('\n');
-    float ldrstat= analogRead(ldrpin);
-    delay(10);
+    int stat= analogRead(ldrpin);
+    delay(100);
+    int stat1 = analogRead(ldrpin1);
+    delay(500);
+
+    digitalWrite(ENA, SPEED);
+    
+    float ldrstat = stat/100;
+    float ldrstat1= stat1/100;
+    Serial.print("ldrstat: ");
     Serial.print(ldrstat, DEC);
     Serial.print('\n');
-    
-    if (ldrstat > 2.3) {
-      //move right
-      digitalWrite(LED,HIGH);
+    Serial.print("ldrstat1: ");
+    Serial.print(ldrstat1, DEC);
+    Serial.print('\n');
+
+    //if not at max light subtract one and compare
+    if (ldrstat1 < 9){
+      ldrstat1 =  ldrstat1 - 1;
+      if (ldrstat > ldrstat1){
+        //Move Right
+        digitalWrite(In1, HIGH);
+        digitalWrite(In2, LOW);  
+      }
+      else if (ldrstat1 > ldrstat){
+        //move left
+        digitalWrite(In1, LOW);
+        digitalWrite(In2, HIGH); 
+      }
+      else {
+        digitalWrite(In1, LOW);
+        digitalWrite(In2, LOW); 
+        digitalWrite(LED, HIGH);
+      }
     }
 
-    else if (ldrstat > 2.7 || ldrstat < 5){
-      //move to the left
-      digitalWrite(LED,HIGH);
-      delay(200);
-      digitalWrite(LED, LOW);
-      delay(200);
-    }
-
+    //
     else {
-      digitalWrite(LED, LOW);
+      if (ldrstat > ldrstat1){
+        //Move Right
+        digitalWrite(In1, HIGH);
+        digitalWrite(In2, LOW);  
+      }
+      else if (ldrstat1 > ldrstat){
+        //move left
+        digitalWrite(In1, LOW);
+        digitalWrite(In2, HIGH); 
+      }
+      else {
+        //dont move
+        digitalWrite(In1, LOW);
+        digitalWrite(In2, LOW); 
+        digitalWrite(LED, HIGH);
+      }
     }
-    delay(500);
 }
-
-On Thu, Dec 6, 2018 at 2:02 PM Irene Duran <iduran@mail.csuchico.edu> wrote:
-
-    //PHOTORESISTORS
-      const int ldrpin = A0;
-     
-
-    //LED
-      const int LED = 13;
-      
-    //MOTOR
-      const int ENA = 7;
-      const int In1 = 8;
-      const int In2 = 9;
-      int SPEED = 100; // scale is 0 to 255
-      
-    void setup() {
-      Serial.begin(9600);  //Begin serial communcation
-      //initialize LDRs and LED
-      pinMode(ldrpin, INPUT);
-      pinMode(LED,OUTPUT);
-
-      //initialize MOTOR
-      pinMode(In1, OUTPUT);
-      pinMode(In2, OUTPUT);
-      pinMode(ENA, OUTPUT);
-    }
-
-    void loop() {
-     
-        float ldrstat= analogRead(ldrpin);
-        delay(10);
-        Serial.print(ldrstat, DEC);
-        if (ldrstat > 2.3) {
-          //move right
-          digitalWrite(LED,HIGH);
-        }
-
-        else if (ldrstat > 2.7 || ldrstat < 5){
-          //move to the left
-          digitalWrite(LED,HIGH);
-          delay(200);
-          digitalWrite(LED, LOW);
-          delay(200);
-        }
-
-        else {
-          digitalWrite(LED, LOW);
-        }
-        delay(500);
-    }
